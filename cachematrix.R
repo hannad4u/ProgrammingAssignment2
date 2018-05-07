3290080bd301726abde85559054d69677757218e
hannad4u <hannad4u@gmail.com>
2018-05-07 18:01:43 +0200

    Assig

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    means <- c()
+    
+    for(monitor in id){
+        path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+        monitor_data <- read.csv(path)
+        interested_data <- monitor_data[pollutant]
+        means <- c(means, interested_data[!is.na(interested_data)])
+    }
+    
+    mean(means)
 }
 
+complete <- function(directory, id = 1:332){
+    results <- data.frame(id=numeric(0), nobs=numeric(0))
+    for(monitor in id){
+        path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+        monitor_data <- read.csv(path)
+        interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+        interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+        nobs <- nrow(interested_data)
+        results <- rbind(results, data.frame(id=monitor, nobs=nobs))
+    }
+    results
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+corr <- function(directory, threshold = 0){
+    cor_results <- numeric(0)
+    
+    complete_cases <- complete(directory)
+    complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
+    
+    if(nrow(complete_cases)>0){
+        for(monitor in complete_cases$id){
+            path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+            #print(path)
+            monitor_data <- read.csv(path)
+            #print(monitor_data)
+            interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+            interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+            sulfate_data <- interested_data["sulfate"]
+            nitrate_data <- interested_data["nitrate"]
+            cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
+        }
+    }
+    cor_results
 }
