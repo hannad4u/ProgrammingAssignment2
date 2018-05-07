780a49d56068bd6b63f592ec4fbbee4c8be92959
hannad4u <hannad4u@gmail.com>
2018-05-07 18:05:38 +0200

    Assig1 updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,30 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-    means <- c()
-    
-    for(monitor in id){
-        path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
-        monitor_data <- read.csv(path)
-        interested_data <- monitor_data[pollutant]
-        means <- c(means, interested_data[!is.na(interested_data)])
+makeCacheMatrix <- function(x = matrix()) {
+    matInv <- NULL
+    set <- function(y) {
+        x <<- y
+        matInv <<- NULL
     }
-    
-    mean(means)
+    get <- function() x
+    setInverse <- function(inverse) matInv <<- inverse
+    getInverse <- function() matInv
+    list(set = set, get = get,
+    setInverse = setInverse,
+    getInverse = getInverse)
 }
 
-complete <- function(directory, id = 1:332){
-    results <- data.frame(id=numeric(0), nobs=numeric(0))
-    for(monitor in id){
-        path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
-        monitor_data <- read.csv(path)
-        interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
-        interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
-        nobs <- nrow(interested_data)
-        results <- rbind(results, data.frame(id=monitor, nobs=nobs))
+
+cacheSolve <- function(x, ...) {
+    m <- x$getInverse()
+    if( !is.null(m) ) {
+        message("Getting cached inv matrix")
+        return(m)
     }
-    results
+    data <- x$get()
+    m <- solve(data) %*% data
+    x$setInverse(m)
+    m
 }
 
-corr <- function(directory, threshold = 0){
-    cor_results <- numeric(0)
-    
-    complete_cases <- complete(directory)
-    complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
-    
-    if(nrow(complete_cases)>0){
-        for(monitor in complete_cases$id){
-            path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
-            #print(path)
-            monitor_data <- read.csv(path)
-            #print(monitor_data)
-            interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
-            interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
-            sulfate_data <- interested_data["sulfate"]
-            nitrate_data <- interested_data["nitrate"]
-            cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
-        }
-    }
-    cor_results
-}
+
+
+
